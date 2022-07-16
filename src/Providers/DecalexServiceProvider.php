72ceba894b785255d32b2c9187b5c784938a0b48
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-07-16 12:59:31 +0300

    routes

--- src/Providers/DecalexServiceProvider.php
+++ src/Providers/DecalexServiceProvider.php
@@ -12,9 +12,10 @@ class DecalexServiceProvider extends ServiceProvider
         /**
          * ROUTES
          */
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/get-config.php');
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/home.php');
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/get-config.php');
         $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/set-locale.php');
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/user-session.php');
 
     }
 }
