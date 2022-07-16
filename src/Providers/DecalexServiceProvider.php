e9b2200a6c163c3ed3d6485cd9ccb4587d3bc078
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-07-16 12:11:43 +0300

    Service provider

---
+++ src/Providers/DecalexServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fhsinchy\Inspire\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DecalexServiceProvider extends ServiceProvider
+{
+
+    public function boot() {
+        dd(__METHOD__);
+    }
+}
