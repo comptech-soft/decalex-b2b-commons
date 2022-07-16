cec5bc19174a85a01d83211911114dd8d690fc25
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-07-16 14:03:46 +0300

    system

--- src/Http/Controllers/System/LoginController.php
+++ src/Http/Controllers/System/LoginController.php
@@ -2,10 +2,9 @@
 
 namespace ComptechSoft\Decalex\Http\Controllers\System;
 
-
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Comptech\Helpers\Response;
+use ComptechSoft\Decalex\Classes\Comptech\Helpers\Response;
 use Cartalyst\Models\User;
 
 class LoginController extends Controller {
