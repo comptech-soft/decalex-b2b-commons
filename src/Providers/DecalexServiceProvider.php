9699c5a90360a5b2defcd9c9351a8cf752bc30cd
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-07-17 02:39:37 +0300

    =Service Provder

--- src/Providers/DecalexServiceProvider.php
+++ src/Providers/DecalexServiceProvider.php
@@ -9,20 +9,25 @@ class DecalexServiceProvider extends ServiceProvider
 
     public function boot() {
 
-        /** System Routes */
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/home.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/get-config.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/set-locale.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/user-session.php');
+        Route::middleware('web')->group(function () {
+            /** System Routes */
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/home.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/get-config.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/set-locale.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/user-session.php');
 
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/configs.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/countries.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/database.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/validation.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/configs.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/countries.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/database.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/system/validation.php');
 
-        /**  Cartalyst Routes */
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/permissions.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/roles.php');
-        $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/users.php');
+            /**  Cartalyst Routes */
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/permissions.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/roles.php');
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/cartalyst/users.php');
+        });
+
+
+        
     }
 }
