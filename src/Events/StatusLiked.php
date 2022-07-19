bb91c09840a9a3c2a429d5536760858ab8479524
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-07-19 22:57:22 +0300

    =First pusher event

---
+++ src/Events/StatusLiked.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace B2B\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StatusLiked {
+
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $user = NULL;
+
+    public $message = NULL;
+
+    public function __construct($user, $message) {
+        
+        $this->user = $user;
+        $this->message = $message;
+
+    }
+
+    public function broadcastOn() {
+        return ['status-liked'];
+    }
+}
