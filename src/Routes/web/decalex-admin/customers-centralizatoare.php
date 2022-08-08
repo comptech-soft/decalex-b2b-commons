11ba19ea53b682aae895febd71b47e8a920f97b1
unknown <ovidiu.andrus@comptech.ro>
2022-08-08 16:57:23 +0300

    custmer centralizatoare

--- src/Routes/web/decalex-admin/customers-centralizatoare.php
+++ src/Routes/web/decalex-admin/customers-centralizatoare.php
@@ -4,8 +4,17 @@ use B2B\Http\Controllers\Decalex;
 
 Route::middleware(['is-authenticated'])->prefix('customers-centralizatoare')->namespace(Decalex::class)->group(function(){
 
+    Route::get('/', 'CustomersCentralizatoareController@index'); 
+
     Route::post('items', 'CustomersCentralizatoareController@getItems');
 
     Route::post('get-summaries', 'CustomersCentralizatoareController@getSummaries');
 
 });
+
+Route::middleware(['is-authenticated'])->prefix('centralizator-raspunsuri')->namespace(Decalex::class)->group(function(){
+    
+    Route::post('items', 'CentralizatorRaspunsuriController@getItems');
+    Route::post('action/{action}', 'CentralizatorRaspunsuriController@doAction');
+});
+
