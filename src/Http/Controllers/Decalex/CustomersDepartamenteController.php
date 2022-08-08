aaa6b1c9eb3c0e7b3c89fd8d3a1d93f9c209df98
unknown <ovidiu.andrus@comptech.ro>
2022-08-09 01:46:29 +0300

    custmer departments

--- src/Http/Controllers/Decalex/CustomersDepartamenteController.php
+++ src/Http/Controllers/Decalex/CustomersDepartamenteController.php
@@ -10,7 +10,7 @@ use B2B\Models\Decalex\CustomerDepartament;
 class CustomersDepartamenteController extends Controller {
     
     public function index(Request $r) {
-        return Response::View('decalex-b2b-commons::~templates.index', asset('apps/departments/index.js'));
+        return Response::View('decalex-b2b-commons::~templates.index', asset('apps/customer-departments/index.js'));
     }
 
     public function getItems(Request $r) {
