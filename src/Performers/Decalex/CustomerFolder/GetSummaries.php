7c59d9c7052b8104bba214f18a0f4aaadfcfb9b9
unknown <ovidiu.andrus@comptech.ro>
2022-08-08 12:41:01 +0300

    Documents summaries

--- src/Performers/Decalex/CustomerFolder/GetSummaries.php
+++ src/Performers/Decalex/CustomerFolder/GetSummaries.php
@@ -11,7 +11,14 @@ class GetSummaries extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        $count_folders = CustomerFolder::where('customer_id', $this->input['customer_id'])->count();
+
+        $this->payload = [
+
+            'folders' => [
+                'count_folders' => $count_folders,
+            ],
+        ];
         
     }
 
