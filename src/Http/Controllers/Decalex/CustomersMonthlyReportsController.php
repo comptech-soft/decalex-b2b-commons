c37f6c8c4406ea6cc02019663c4a99307aa97d9c
unknown <ovidiu.andrus@comptech.ro>
2022-08-08 10:18:01 +0300

    Customer customers-monthly-reports

---
+++ src/Http/Controllers/Decalex/CustomersMonthlyReportsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace B2B\Http\Controllers\Decalex;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use B2B\Classes\Comptech\Helpers\Response;
+
+class PersonsController extends Controller {
+    
+    public function index(Request $r) {
+        return Response::View('decalex-b2b-commons::~templates.index', asset('apps/monthly-reports/index.js'));
+    }
+    
+}
