b9b37b23faf215d1e8f9d434034d8b79e51a16a8
unknown <ovidiu.andrus@comptech.ro>
2022-08-08 10:15:33 +0300

    Customer customers-monthly-reports

--- src/Providers/DecalexServiceProvider.php
+++ src/Providers/DecalexServiceProvider.php
@@ -32,7 +32,8 @@ class DecalexServiceProvider extends ServiceProvider
             /** Decalex commons routes */
             $this->loadRoutesFrom(__DIR__ . '/../Routes/web/decalex-admin/customers-departamente.php');
             $this->loadRoutesFrom(__DIR__ . '/../Routes/web/decalex-admin/customers-persons.php');
-            
+            $this->loadRoutesFrom(__DIR__ . '/../Routes/web/decalex-admin/customers-monthly-reports.php');
+
             if(config('app.platform') == 'admin')
             {
                 /**  Decalex admin Routes */
