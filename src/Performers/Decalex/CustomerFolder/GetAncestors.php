a7426a45f51d01212939b6cbadafa7624c6a53f0
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-11 00:51:19 +0300

    Folders  ancestors

--- src/Performers/Decalex/CustomerFolder/GetAncestors.php
+++ src/Performers/Decalex/CustomerFolder/GetAncestors.php
@@ -5,14 +5,15 @@ namespace B2B\Performers\Decalex\CustomerFolder;
 use B2B\Classes\Comptech\Helpers\Perform;
 
 // use B2B\Models\Decalex\CustomerFile;
-// use B2B\Models\Decalex\CustomerFolder;
+use B2B\Models\Decalex\CustomerFolder;
 
 class GetAncestors extends Perform {
 
     public function Action() {
 
-        dd($this->input);
+        $node = CustomerFolder::find($this->input['folder_id']);
         
+        $this->payload['ancestors'] = $node->ancestors;
     }
 
 }
