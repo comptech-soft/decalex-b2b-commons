afcb011b87771c6a96bfae7c5debb2ec49bf086b
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-16 02:20:31 +0300

    Trimiteri insert

--- src/Traits/Decalex/Trimitere/Actions.php
+++ src/Traits/Decalex/Trimitere/Actions.php
@@ -16,9 +16,9 @@ trait Actions {
         $result = [
             'number' => 'required',
             'date' => 'required|date',
-            'completed_from' => 'required|date',
-            'completed_to' => 'required|date',
-            'effective_time' => 'required|numeric'
+            // 'completed_from' => 'required|date',
+            // 'completed_to' => 'required|date',
+            // 'effective_time' => 'required|numeric'
             
         ];
 
@@ -31,9 +31,9 @@ trait Actions {
         $result = [
             'number.required' => 'Numărul trebuie completat',
             'date.required' => 'Data trebuie completată',
-            'completed_from.required' => 'Intervalul trebuie completat',
-            'completed_to.required' => 'Intervalul trebuie completat',
-            'effective_time.required' => 'Timpul de lucru trebuie completat',
+            // 'completed_from.required' => 'Intervalul trebuie completat',
+            // 'completed_to.required' => 'Intervalul trebuie completat',
+            // 'effective_time.required' => 'Timpul de lucru trebuie completat',
             
         ];
 
