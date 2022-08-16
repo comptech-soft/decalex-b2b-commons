56a335c5ea643f6ab2ac353b1c846295bff49b99
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-16 14:26:39 +0300

    open Knolyx course

--- src/Performers/Decalex/Curs/OpenKnolyxCourse.php
+++ src/Performers/Decalex/Curs/OpenKnolyxCourse.php
@@ -3,18 +3,49 @@
 namespace B2B\Performers\Decalex\Curs;
 
 use B2B\Classes\Comptech\Helpers\Perform;
+use B2B\Models\Cartalyst\User;
 
 class OpenKnolyxCourse extends Perform {
 
+    public static function CreateKnolyxUser($user) {
+        if( ! $user['k_id'] )
+        {
+            $kUserProvision =  \Http::withHeaders([
+                'X-Project-Id' => config('knolyx.project_id'),
+                'X-Api-Key' => config('knolyx.app_key')
+            ])
+            ->put(
+                config('knolyx.endpoint') . 'user/provision',
+                [
+                    'firstName' => $user->first_name, 
+                    'lastName' => $user->last_name,
+                    'email' => $user->email,
+                    'disableWelcomeEmail' => TRUE,
+                ]
+            )
+            ->json();
+
+            if( array_key_exists('id', $kUserProvision))
+            {
+                $user->k_id = $kUserProvision['id'];
+                $user->save();
+            }
+        }
+    }
+
     public function Action() {
         
-        dd(__METHOD__, $this->input);
-        // $response =  \Http::withHeaders([
-        //     'X-Project-Id' => config('knolyx.project_id'),
-        //     'X-Api-Key' => config('knolyx.app_key')
-        // ])->get(config('knolyx.endpoint') . 'course?pagination.page=0&pagination.size=20');
+        $user = array_key_exists('user_id', $this->input) ? User::find($this->input['user_id']) : \Sentinel::check();
+        
+        self::CreateKnolyxUser($user);
 
-        // $this->payload['cursuri'] = $response->json(); 
+        if( ! $user['k_id'] ) 
+        {
+            throw new \Exception('NU am utilizator Knolyx.');
+        }
+        
+        
+        
     }
 
 } 
