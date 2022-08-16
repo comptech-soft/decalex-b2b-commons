c1333858b32a21d0e7db3eb4c688d53155035527
Andrus Ovidiu <ovidiu.andrus@comptech.ro>
2022-08-16 14:57:43 +0300

    open Knolyx course

--- src/Performers/Decalex/Curs/OpenKnolyxCourse.php
+++ src/Performers/Decalex/Curs/OpenKnolyxCourse.php
@@ -4,6 +4,7 @@ namespace B2B\Performers\Decalex\Curs;
 
 use B2B\Classes\Comptech\Helpers\Perform;
 use B2B\Models\Cartalyst\User;
+use B2B\Models\Decalex\Curs;
 
 class OpenKnolyxCourse extends Perform {
 
@@ -33,6 +34,31 @@ class OpenKnolyxCourse extends Perform {
         }
     }
 
+    public static function GetCourseRole($course_id) {
+        $response =  \Http::withHeaders([
+            'X-Project-Id' => config('knolyx.project_id'),
+            'X-Api-Key' => config('knolyx.app_key')
+        ])
+        ->get(config('knolyx.endpoint') . 'business-rule/course/' . $course_id)
+        ->json();
+        return $response[0];
+    }
+
+    public static function SetCourseRole($course_id, $courseRole) {
+        $response =  \Http::withHeaders([
+            'X-Project-Id' => config('knolyx.project_id'),
+            'X-Api-Key' => config('knolyx.app_key')
+        ])
+        ->post(
+            config('knolyx.endpoint') . 'business-rule/course/' . $course_id,  
+            [$courseRole]
+        );
+
+        dd($courseRole, $response->json());
+
+        // ->json();
+    }
+
     public function Action() {
         
         $user = array_key_exists('user_id', $this->input) ? User::find($this->input['user_id']) : \Sentinel::check();
@@ -43,8 +69,23 @@ class OpenKnolyxCourse extends Perform {
         {
             throw new \Exception('NU am utilizator Knolyx.');
         }
+
+        $course_id = Curs::find($this->input['curs_id'])->k_id;
+
+        $courseRole = self::GetCourseRole($course_id);
+
+
+        if( ! array_key_exists('USER', $courseRole['associations']) )
+        {
+            $courseRole['associations']['USER'] = [];
+        }
         
-        
+        if( ! in_array($user['k_id'], $courseRole['associations']['USER']) )
+        {
+            $courseRole['associations']['USER'][] = $user['k_id'];
+            self::SetCourseRole($course_id, $courseRole);
+        }
+
         
     }
 
